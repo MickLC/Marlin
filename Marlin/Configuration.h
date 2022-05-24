82a6332f0c309560acaf127b0e78cc5d3b052cbf
Mickey Chandler <github@spamtacular.com>
2022-05-24 17:02:54 -0500

    Decreased the bed size to 230x230

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1487,8 +1487,8 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 235
-#define Y_BED_SIZE 235
+#define X_BED_SIZE 230
+#define Y_BED_SIZE 230
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
